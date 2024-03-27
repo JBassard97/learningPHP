cf6d2e7eac55ce5adba9473e93e00fde490df10a
Jbassard <jonathanacciarito@gmail.com>
2024-03-27 00:24:52 -0400

    began taking notes from w3schools

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,3 +1,20 @@
-<?php 
+
+<?php
+// PHP: Hypertext Preprocessor
+// Any comments left outside the <?php tags will also be outputted
 echo "Hello World";
 ?>
+
+
+<?php
+//  Declare variables starting with $
+//  Every line ends with ;
+$txt = "PHP";
+echo "$txt is pretty neat";
+?>
+
+<?php
+// Instead of typing php --version, this function returns the version you're using
+echo phpversion();
+?>
+
