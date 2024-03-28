3240b320d89e71c7947195af1750803664d66a22
Jbassard <jonathanacciarito@gmail.com>
2024-03-27 22:40:11 -0400

    added more notes

--- HelloWorld.php
+++ HelloWorld.php
@@ -10,6 +10,8 @@ echo "Hello World";
 // Instead of typing php --version, phpversion() returns the version you're using
 // echo phpversion();
 
+// ! Variables
+
 //  Declare variables starting with $
 //  Every line ends with ;
 $txt = "PHP";
@@ -29,7 +31,7 @@ var_dump([1, "two", null, true]);
 // You can assign the same value to multiple variables
 $x = $y = $z = "Fruit";
 
-// ! The above variables are in 'global' scope. Unlike JavaScript, these are NOT usable inside a function
+// ? The above variables are in 'global' scope. Unlike JavaScript, these are NOT usable inside a function
 
 // Variables declared inside a function have 'local' scope, and are ONLY usable inside that function
 
@@ -58,7 +60,105 @@ function myGlobalsTest()
 myGlobalsTest();
 echo $y; // outputs 15
 
+// ! Common String methods
+
 // Find the length of a string with strlen()
 echo strlen("Hello world!");
 
+// Find the number of words in a string
+echo str_word_count("word count");
+
+// Uppercase the entire string
+echo strtoupper("uppercase");
+
+// Lowercase the entire string
+echo strtolower("LOWERCASE");
+
+// Uppercase the first letter of a string
+echo ucfirst("uPPERCASE first letter");
+
+// Uppercase the first letter of every word in a string
+echo ucwords("tITLE cASE");
+
+// Concat strings with the . operator
+$x = "Hello";
+$y = "World";
+$z = $x . $y;
+echo $z;
+// or simply
+$z = "$x $y";
+echo $z;
+
+// Slice a string from a starting index to a specified length
+$x = "Hello World!";
+echo substr($x, 6, 5);
+// without the end value it'll slice until the end
+echo substr($x, 6);
+
+// To insert characters that are illegal in a string, use an escape character.
+$x = "We are the so-called \"Vikings\" from the north.";
+
+// ! Common Number Methods
+
+// Returns the value of PI
+echo (PI());
+
+// Find the lowest or highest value in a list of arguments:
+echo (min(0, 150, 30, 20, -8, -200));
+echo (max(0, 150, 30, 20, -8, -200));
+
+// Returns the absolute (positive) value of a number
+echo (abs(-6.7));
+
+// Returns the square root of a number:
+echo (sqrt(64));
+
+// Rounds a floating-point number to its nearest integer:
+// ? 0.4 to the floor, 0.5 to the sky
+echo (round(0.60));
+echo (round(0.49));
+
+// Generates a random number:
+echo (rand());
+// or control it by specifying lowest and highest integers:
+echo (rand(10, 100));
+
+// ! Constants (important)
+// define(name, value, case-insensitive);
+// ? A constant does not start with $, and unlike variables is available EVERYWHERE
+// ? Default for 'case-insensitive' arg is false, but can be specified as true
+
+define("GREETING", "Welcome to W3Schools.com!");
+echo GREETING;
+
+// ? OR use const keyword:
+const MYCAR = "Volvo";
+echo MYCAR;
+
+// ? define() is better than const, as it has a case-insensitive opeion and can be used in any scope freely
+
+// ! Magic Constants (that's their name lol)
+// __CLASS__	If used inside a class, the class name is returned.	
+// __DIR__	The directory of the file.	
+// __FILE__	The file name including the full path.	
+// __FUNCTION__	If inside a function, the function name is returned.	
+// __LINE__	The current line number.	
+// __METHOD__	If used inside a function that belongs to a class, both class and function name is returned.	
+// __NAMESPACE__	If used inside a namespace, the name of the namespace is returned.	
+// __TRAIT__	If used inside a trait, the trait name is returned.
+
+// ! Operators
+// Consistent with other languages, except we have spaceship operator <=>
+
+$x = 5;
+$y = 10;
+echo ($x <=> $y); //returns -1 since x is less than y
+$x = 10;
+$y = 10;
+echo ($x <=> $y); // returns 0 because values are equal
+$x = 15;
+$y = 10;
+echo ($x <=> $y); // returns 1 because $x is greater than $y
+
+// ? PHP also offers <> as 'loose not equal' as well as !=
 ?>
